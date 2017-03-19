688b5962a9d364022943d9816a4cbe12b7f205aa
Tom Hutchison <Hutchy68@gmail.com>
2017-03-19 13:53:37 -0400

    More improvements for echo notifications and the placement of them

--- assets/scripts/pivot.js
+++ assets/scripts/pivot.js
@@ -11,7 +11,6 @@ jQuery(document).ready(function() {
   
   // The Echo extension puts an item in personal tools that Foreground really should have in the top menu
   // to make this easier, we move it here and loaded earlier to speed up transform
-  jQuery("#pt-notifications").prependTo("#echo-notifications-alerts");
   jQuery("#pt-notifications-message").prependTo("#echo-notifications-messages");
   jQuery("#pt-notifications-alert").prependTo("#echo-notifications-alerts");
   
